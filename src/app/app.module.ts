fc8d64e7d6e3df16d02bfff6fa803782564aa8da
PrakharIITB <105409808+PrakharIITB@users.noreply.github.com>
2022-12-01 22:18:16 +0530

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Components
+import { HeaderComponent } from './header/header.component';
+import { SelectComponent } from './select/select.component';
+
+//Material UI
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { Section2Component } from './section2/section2.component';
+import { ParametersComponent } from './parameters/parameters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SelectComponent,
+    Section2Component,
+    ParametersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/parameters/parameters.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {Chart} from 'highcharts'
+
+@Component({
+  selector: 'app-parameters',
+  templateUrl: './parameters.component.html',
+  styleUrls: ['./parameters.component.css']
+})
+export class ParametersComponent {
+  
+}
